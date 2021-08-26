6d78a2210449d9079837af3b33a0457e21ca965d
evedvat <vedran.grgo.vatavuk@ericsson.com>
2021-08-26 15:00:22 +0200

    added javadoc to Results

--- src/main/java/hr/com/vgv/asyncunit/Results.java
+++ src/main/java/hr/com/vgv/asyncunit/Results.java
@@ -6,6 +6,12 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Thread execution results. It can receive execution results from different threads and wait until all
+ * executions are received or until timeout expires.
+ *
+ * @author Vedran Vatavuk
+ */
 public class Results
 {
     private final Queue<Throwable> errors = new ConcurrentLinkedQueue<>();
@@ -14,32 +20,66 @@ public class Results
 
     private final Semaphore semaphore = new Semaphore(0);
 
+    /**
+     * Signal successful execution of a thread.
+     */
     public void addSuccess()
     {
         semaphore.release();
     }
 
+    /**
+     * Add throwable that caused a thread to fail.
+     */
     public void addFailure(Throwable throwable)
     {
         errors.add(throwable);
         semaphore.release();
     }
 
+    /**
+     * Waits for a single thread execution result. It will wait until interrupted.
+     *
+     * @throws InterruptedException If interrupted
+     */
     public void await() throws InterruptedException
     {
         await(0);
     }
 
+    /**
+     * Waits for a single thread execution result. If no result received until given period, it will raise
+     * AssertionError.
+     *
+     * @param period Time period in milliseconds
+     * @throws InterruptedException If interrupted
+     */
     public void await(long period) throws InterruptedException
     {
         await(period, TimeUnit.MILLISECONDS, 1);
     }
 
+    /**
+     * Waits for a given number of thread execution results. If a number of received results is not equal to
+     * numOfExecutions, it will raise AssertionError.
+     *
+     * @param period Time period in milliseconds
+     * @throws InterruptedException If interrupted
+     */
     public void await(long period, int numOfExecutions) throws InterruptedException
     {
         await(period, TimeUnit.MILLISECONDS, numOfExecutions);
     }
 
+    /**
+     * Waits for a given number of thread execution results. If a number of received results is not equal to
+     * numOfExecutions, it will raise AssertionError.
+     *
+     * @param period Time period
+     * @param timeUnit Time unit
+     * @param numOfExecutions Number of expected thread executions
+     * @throws InterruptedException If interrupted
+     */
     public void await(long period, TimeUnit timeUnit, int numOfExecutions) throws InterruptedException
     {
         synchronized (this)
@@ -71,6 +111,12 @@ public class Results
         }
     }
 
+    /**
+     * Constructs error message.
+     *
+     * @param expected Expected number of executions
+     * @return Error message
+     */
     private String notEnoughExecutions(int expected)
     {
         return String.format(
@@ -78,6 +124,9 @@ public class Results
         );
     }
 
+    /**
+     * Throws throwable if any of threads ended with an exception.
+     */
     private void throwOnError()
     {
         try
